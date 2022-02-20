e659abc557263ef1d506accf1e49d971c6ae2529
Matheus Nugas <matheusnugas@gmail.com>
2022-02-20 14:00:18 -0300

    Projects page added/updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Contact from "./pages/Contact";
 import HomePage from "./pages/HomePage";
@@ -9,15 +9,19 @@ import Footer from "./components/Footer";
 import About from "./pages/About";
 
 function App() {
+  const location = useLocation();
+  const path = location.pathname;
   return (
-    <BrowserRouter>
+    <div>
       <NavBar />
       <Route exact path="/" component={HomePage} />
       <Route exact path="/projects" component={Projects} />
       <Route exact path="/contact" component={Contact} />
       <Route exact path="/about" component={About} />
-      <Footer />
-    </BrowserRouter>
+      {path === "/" || path === "/about" || path === "/contact" ? (
+        <Footer />
+      ) : null}
+    </div>
   );
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import todoListImg from "../images/todolistimg.png";
+import "./Projects.css";
 function Projects() {
-  return <div>Projects page</div>;
+  return (
+    <div className="projectsWrapper">
+      <a href="https://mlncontexttodolist.vercel.app/">
+        <div className="projectDiv">
+          <img src={todoListImg} alt="projectImg" />
+          <p>React/Context API Todo-List</p>
+        </div>
+      </a>
+    </div>
+  );
 }
 
 export default Projects;
