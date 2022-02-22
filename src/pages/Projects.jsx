87696d4926f0f9ff7d3bafac17a76bf3518b1980
Matheus Nugas <matheusnugas@gmail.com>
2022-02-22 14:56:48 -0300

    Money saver project added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import todoListImg from "../images/todolistimg.png";
+import walletImg from "../images/walletImage.png";
 import "./Projects.css";
 function Projects() {
   return (
@@ -8,6 +9,12 @@ function Projects() {
         <div className="projectDiv">
           <img src={todoListImg} alt="projectImg" />
           <p>React/Context API Todo-List</p>
+        </div>
+      </a>
+      <a href="https://mlnreduxwallet.vercel.app//">
+        <div className="projectDiv">
+          <img src={walletImg} alt="projectImg" />
+          <p>React/Redux Expense Tracker</p>
         </div>
       </a>
     </div>
